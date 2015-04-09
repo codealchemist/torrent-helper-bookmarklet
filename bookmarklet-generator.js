1549457110ab861f62f106ad0462e09fc678e480
Alberto Miranda <codealchemist@gmail.com>
2015-04-09 10:44:45 -0300

    fixed script url; updated bookmarklet name

--- bookmarklet-generator.js
+++ bookmarklet-generator.js
@@ -1,13 +1,13 @@
 // BOOKMARKLET GENERATOR
-var scriptUrl = '//cdn.rawgit.com/codealchemist/torrent-helper-bookmarklet/master/bookmarklet-generator.js',
-	js="javascript:(function(){ var protocol = location.protocol;var bookmarkletUrl = protocol + scriptUrl;var script = document.createElement('script');script.src = bookmarkletUrl;document.body.appendChild(script); })()";
+var script = "var scriptUrl='//cdn.rawgit.com/codealchemist/torrent-helper-bookmarklet/master/torrent-helper.bookmarklet.js';",
+	js="javascript:(function(){ " + script + "var protocol = location.protocol;var bookmarkletUrl = protocol + scriptUrl;var script = document.createElement('script');script.src = bookmarkletUrl;document.body.appendChild(script); })()";
 	div = document.createElement('div'),
 	style="position:fixed; margin: auto; top: 20px; left:0; right:0; padding: 20px; width: 400px; border: 1px solid #ccc; background: white; border-radius: 5px; color: black;";
 
 var html = '' +
 	'<div style="' + style + '">' +
 	'<div><strong>Install</strong> by dragging the following link to your bookmarks bar:</div>' +
-	'<a href="' + js + '">Torrent Helper</a> | ' +
+	'<a href="' + js + '">Torrent Helper (github)</a> | ' +
 	'<a href="javascript:void(0)" onclick="document.body.removeChild(this.parentElement.parentElement);">close</a>' +
 	'</div>';
 
