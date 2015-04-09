e6da2cfd9ddba44378c61a04b3d79aea5dd3c84b
Alberto Miranda <codealchemist@gmail.com>
2015-04-09 10:26:54 -0300

    fixed styles to work crossbrowser

--- bookmarklet-generator.js
+++ bookmarklet-generator.js
@@ -2,12 +2,14 @@
 var scriptUrl = '//raw.githubusercontent.com/codealchemist/torrent-helper-bookmarklet/master/torrent-helper.bookmarklet.js',
 	js="javascript:(function(){ var protocol = location.protocol;var bookmarkletUrl = protocol + scriptUrl;var script = document.createElement('script');script.src = bookmarkletUrl;document.body.appendChild(script); })()";
 	div = document.createElement('div'),
-	div.style="position:fixed; margin: auto; top: 20px; left:0; right:0; padding: 20px; width: 400px; border: 1px solid #ccc; background: white; border-radius: 5px; color: black;";
+	style="position:fixed; margin: auto; top: 20px; left:0; right:0; padding: 20px; width: 400px; border: 1px solid #ccc; background: white; border-radius: 5px; color: black;";
 
 var html = '' +
+	'<div style="' + style + '">' +
 	'<div><strong>Install</strong> by dragging the following link to your bookmarks bar:</div>' +
 	'<a href="' + js + '">Torrent Helper</a> | ' +
-	'<a href="javascript:void(0)" onclick="document.body.removeChild(this.parentElement);">close</a>';
+	'<a href="javascript:void(0)" onclick="document.body.removeChild(this.parentElement.parentElement);">close</a>' +
+	'</div>';
 
 div.innerHTML = html;
 document.body.appendChild(div);
